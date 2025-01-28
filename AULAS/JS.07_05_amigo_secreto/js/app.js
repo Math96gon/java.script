f63193718944f91c680b41ab8ac783d3c7763b2c
Math96gon <mathgon96@gmail.com>
2025-01-28 17:02:00 -0300

    java 02

---
+++ AULAS/JS.07_05_amigo_secreto/js/app.js
@@ -0,0 +1,45 @@
+let amigos[];
+
+function adicionar(){
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigo');
+    amigos.push(amigos.value);
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    }else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+    
+}
+
+function sortear () {
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+     for ( let i = 0; i < amigos.length; i++) {
+
+        if ( i == amigos.length - 1){
+        sorteio.innerHTML = sorteiro.innerHTML + amigos[i] + ' -->' + amigos [0] + '<br>';
+        }else{
+        sorteio.innerHTML = sorteiro.innerHTML + amigos[i] + ' -->' + amigos [i + 1] + '<br>';
+        }
+ }
+}
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random()* indice);
+
+        [lista[ indice -1], lista[indiceAleatorio]]=
+            [lista[indiceAleatorio]], lista[indice - 1];
+    }
+}
+
+function reiniciar () {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById9('lista-sorteio').innerHTML = '';
+    
+
+}
